f9051e58932cd0ca4fff05e73f6e0669254190fa
dmalahovev-lab <dmalahovev@gmail.com>
2026-07-09 23:30:52 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,48 +2,24 @@ const CACHE_NAME = 'danumes-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/style.css',
+  '/app.js',
+  '/socket.io/socket.io.js',
+  '/manifest.json',
 ];
 
-// Установка service worker
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-// Перехват запросов
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-// Обработка push-уведомлений
-self.addEventListener('push', event => {
-  const data = event.data.json();
-  const options = {
-    body: data.body,
-    icon: '/icon-192.png',
-    badge: '/icon-72.png',
-    vibrate: [200, 100, 200],
-    sound: '/notification.mp3',
-    data: {
-      url: data.url || '/'
-    }
-  };
-  
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
-  );
-});
-
-// Клик по уведомлению
-self.addEventListener('notificationclick', event => {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow(event.notification.data.url)
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
