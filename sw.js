84667358cfe115d10c2058f4c49b70bb02d813c2
dmalahovev-lab <dmalahovev@gmail.com>
2026-07-10 17:14:28 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ const urlsToCache = [
   '/index.html',
   '/style.css',
   '/app.js',
-  '/socket.io/socket.io.js',
   '/manifest.json',
+  '/socket.io/socket.io.js'
 ];
 
 self.addEventListener('install', (event) => {
@@ -14,12 +14,32 @@ self.addEventListener('install', (event) => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('/');
+        }
+      });
     })
   );
 });
